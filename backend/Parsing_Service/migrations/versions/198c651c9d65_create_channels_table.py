ccd181d604b695b005ad6240d8639b2a44a2db8f
Dimitry Safronov <sda201018@gmail.com>
2026-02-16 20:02:59 +0300

    edit alembic config

--- backend/Parsing_Service/migrations/versions/198c651c9d65_create_channels_table.py
+++ backend/Parsing_Service/migrations/versions/198c651c9d65_create_channels_table.py
@@ -1,10 +1,11 @@
 """create channels table
 
 Revision ID: 198c651c9d65
-Revises: 
+Revises:
 Create Date: 2026-02-16 19:54:30.123009
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,17 +13,21 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '198c651c9d65'
+revision: str = "198c651c9d65"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
+    op.create_table(
+        "channels",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.String(255), nullable=False, unique=True),
+    )
     pass
 
 
 def downgrade() -> None:
-    """Downgrade schema."""
+    op.drop_table("channels")
     pass
