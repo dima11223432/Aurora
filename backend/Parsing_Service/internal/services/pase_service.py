113ce6143867acff82efdf65e32dbdb7170e9196
Yan <yan@gmail.com>
2026-02-20 22:55:23 +0300

    add metod last_post in parse_service

--- backend/Parsing_Service/internal/services/pase_service.py
+++ backend/Parsing_Service/internal/services/pase_service.py
@@ -22,3 +22,49 @@ class ParserService():
             self.is_connect = False
         except:
             print("err in disconnect")
+
+    def last_post(self, us_chanal):
+        if not self.is_connect:
+            self.connect()
+        
+        try:
+            channel = await self.client.get_entity(us_chanal)
+            message = await self.client.get_messages(channel, limit=1)
+
+            if not message:
+                print("err: not messages")
+            
+            post_data = {
+                "id": message[0].id,
+                "date": message[0].date
+                "text": message.text
+            }
+
+            return post_data 
+        except:
+            print("err in message")
+            
+"""
+    def pars_posts(self, channels_usernams):
+
+        if not self.is_connect:
+            await self.connect()
+        
+        for channel_un in channels_usernams:
+            list_channels = []
+            try:
+                channel = await self._get_chennel_entity(channel_un)
+                list_channels.append(channel)
+            except:
+                print(f"cant add channel {channel_un}")
+
+        if not list_channels:
+            print("no channels")
+            return 
+        
+        @self.client.on(events.NewMessage(chats = list_channels))
+        async def new_message(event):
+            message = event.message
+            channel = await event.get_chat()
+
+"""
