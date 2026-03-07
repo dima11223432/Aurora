c319e9f41ef6dc2660df9f88304c5e621df350c8
Ubenben <safka.bur@gmail.com>
2026-03-08 02:20:19 +0300

    Added new method in server.go

--- backend/Recommendation_Service/internal/grpc/userDataProvider/server.go
+++ backend/Recommendation_Service/internal/grpc/userDataProvider/server.go
@@ -29,6 +29,27 @@ func Register(gRPC *grpc.Server, userDataProvider UserDataProvider) {
 	})
 }
 
-func GetUserPriorityChanneld(ctx context.Context, req *ssov1.GetUserPriotiryChannelsRequest) ([]models.PriorityChannel, error) {
-	return nil, errors.New("Not implemented")
+func (s *serverAPI) GetUserPriorityChanneld(ctx context.Context,req *ssov1.GetUserPriotiryChannelsRequest) (*ssov1.GetUserPriotiryChannelsResponse, error) {
+
+	const op = "internal.transport.grpc.serverAPI.GetUserPriorityChanneld"
+
+	if req.GetUserId() == 0 {
+		return nil, errors.New(op + ": user_id is empty")
+	}
+
+	channels, err := s.userDataProvider.GetUserPriorityChanneld(ctx, req.GetUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &ssov1.GetUserPriotiryChannelsResponse{}
+
+	for _, ch := range channels {
+		resp.Channels = append(resp.Channels, &ssov1.PriorityChannel{
+			ChannelId: ch.ChannelID,
+			Priority:  int32(ch.Priority),
+		})
+	}
+
+	return resp, nil
 }
