95aecbd2e6586db4bc6fa7c1ec356552b509aeca
Dimitry Safronov <sda201018@gmail.com>
2026-03-12 22:23:37 +0300

    perform a little code review in grpc service

--- backend/Recommendation_Service/internal/grpc/userDataProvider/server.go
+++ backend/Recommendation_Service/internal/grpc/userDataProvider/server.go
@@ -5,7 +5,7 @@ import (
 	"github.com/samber/lo"
 	ssov1 "recommendationService/api/gen/v1"
 	"recommendationService/internal/domain/models"
-	grpcErr "recommendationService/internal/grpc"
+	// grpcErr "recommendationService/internal/grpc"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -30,25 +30,14 @@ func Register(gRPC *grpc.Server, userDataProvider UserDataProvider) {
 	})
 }
 
-func (s *serverAPI) GetUserPriorityChanneld(ctx context.Context, req *ssov1.GetUserPriorityChannelsRequest) (*ssov1.GetUserPriorityChannelsResponse, error) {
-
-	const op = "internal.transport.grpc.serverAPI.GetUserPriorityChanneld"
-
-	if req.GetUserId() == emptyValue {
-		return nil, grpcErr.ErrUserIDEmpty
-	}
-
+func (s *serverAPI) GetUserPriorityChannels(ctx context.Context, req *ssov1.GetUserPriorityChannelsRequest) (*ssov1.GetUserPriorityChannelsResponse, error) {
 	channels, err := s.userDataProvider.GetUserPriorityChannels(ctx, req.GetUserId())
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Internal, "internal error")
 	}
-
-	for _, ch := range channels {
-		resp.Channels = append(resp.Channels, string{
-			ChannelId: ch.ChannelID,
-			Priority:  int32(ch.Priority),
-		})
-	}
-    resp := &ssov1.GetUserPriorityChannelsResponse{}
-	return resp, nil
+	return &ssov1.GetUserPriorityChannelsResponse{
+		Channels: lo.Map(channels, func(item models.PriorityChannel, _ int) string {
+			return item.Channel
+		}),
+	}, nil
 }
