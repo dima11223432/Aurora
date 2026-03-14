14558f2bf998d37ee3db439f7a844afe95ca129e
Dimitry Safronov <sda201018@gmail.com>
2026-03-14 19:05:36 +0300

    inicialize redis controller

--- backend/Recommendation_Service/internal/storage/redis/redis.go
+++ backend/Recommendation_Service/internal/storage/redis/redis.go
@@ -1,7 +1,53 @@
 package redis
 
-import "github.com/redis/go-redis/v9"
+import (
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type RedisController struct {
-	redis *redis.Client
+	redis      *redis.Client
+	DefaultTTl time.Duration
+}
+
+func NewRedisController(addr string, password string, db int, protocol int, ttl time.Duration) *RedisController {
+	return &RedisController{
+		redis: redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: password,
+			DB:       db,
+			Protocol: protocol,
+		}),
+		DefaultTTl: ttl,
+	}
+}
+
+func (r *RedisController) SetValue(ctx context.Context, key string, value interface{}, ttl ...time.Duration) error {
+	const op = "Cahce_Service.internal.storage.redis.SetValue"
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if len(ttl) > 0 {
+		return r.redis.Set(ctx, key, data, ttl[0]).Err()
+	}
+	return r.redis.Set(ctx, key, data, r.DefaultTTl).Err()
+}
+
+func (r *RedisController) GetValue(ctx context.Context, key string) (interface{}, error) {
+	const op = "Cahce_Service.internal.storage.redis.GetValue"
+	data, err := r.redis.Get(ctx, key).Bytes()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return nil, storage.ErrCacheMiss
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return data, nil
+}
+
+func (r *RedisController) Ping(ctx context.Context) error {
+	return r.redis.Ping(ctx).Err()
 }
--- backend/Recommendation_Service/internal/storage/storage.go
+++ backend/Recommendation_Service/internal/storage/storage.go
@@ -6,6 +6,7 @@ var (
 	ErrUserExists      = errors.New("user already exists")
 	ErrUserNotFound    = errors.New("user not found")
 	ErrAppNotFound     = errors.New("app not found")
+	ErrCacheMiss       = errors.New("cache miss")
 	ErrChannelsEmpty   = errors.New("channels list cannot be empty")
 	ErrChannelExists   = errors.New("channel already added for this user")
 	ErrChannelNotFound = errors.New("channel not found")
