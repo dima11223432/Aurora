cdba778381d683f669218afe738a4be5ebf59752
FrogaIo <nickosipovosipov@yandex.ru>
2026-03-15 18:32:14 +0300

    Changed the nvidia AI to the Qwen using the yandex ai studio

--- backend/Analitic_Service/AI_handler.py
+++ backend/Analitic_Service/AI_handler.py
@@ -2,16 +2,16 @@
 import threading
 
 from DeepSeek import answer as ds
-from NvidiaAI import answer as nv
+from QwenAI import answer as qw
 from StepAI import answer as st 
 from YandexAI import answer as ya
 
 def AI_handler(context):
     AI_answer = {}
-    AI_list = {"ds", "nv", "st", "ya" }
+    AI_list = {"ds", "qw", "st", "ya" }
     for i in AI_list:
         AI_answer[i] = globals()[i](context).replace(" \n", "")
     return AI_answer
 
-test = AI_handler("Nvidia перестанет производить видеокарты")
+test = AI_handler("Apple продаёт все заводы.")
 print(test)
