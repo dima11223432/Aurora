346885f955a805e9ce050ae7bb152cef7aa12916
FrogaIo <nickosipovosipov@yandex.ru>
2026-03-15 19:45:29 +0300

    Added the prompt ineqtion security, also prepared the output fo rthe futurie

--- backend/Analitic_Service/AI_handler.py
+++ backend/Analitic_Service/AI_handler.py
@@ -7,10 +7,10 @@ from StepAI import answer as st
 from YandexAI import answer as ya
 
 def AI_handler(context):
-    AI_answer = {}
+    AI_answer = {"ds": {}, "qw": {}, "st": {}, "ya":{}}
     AI_list = {"ds", "qw", "st", "ya" }
     for i in AI_list:
-        AI_answer[i] = globals()[i](context).replace(" \n", "")
+        AI_answer[i]["answer"] = globals()[i](context).replace(" \n", "")
     return AI_answer
 
 test = AI_handler("Apple продаёт все заводы.")
