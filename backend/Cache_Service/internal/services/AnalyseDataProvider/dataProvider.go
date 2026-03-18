78f133f1b9e770c9475fb4aa6d8f6f40d05c44c4
Dimitry Safronov <sda201018@gmail.com>
2026-03-18 20:22:47 +0000

    Feature/recommendation service redis

---
+++ backend/Cache_Service/internal/services/AnalyseDataProvider/dataProvider.go
@@ -0,0 +1,44 @@
+package analyseDataProvider
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+)
+
+type AnasyledDataProvider interface {
+	SetValue(ctx context.Context, key string, value interface{}, ttl ...time.Duration) error
+	GetValue(ctx context.Context, key string) (interface{}, error)
+}
+
+type RedisService struct {
+	log      *slog.Logger
+	provider AnasyledDataProvider
+	TokenTTL time.Duration
+}
+
+func NewRedisService(log *slog.Logger, analyseDataProvider AnasyledDataProvider, tokenTTL time.Duration) *RedisService {
+	return &RedisService{
+		log:      log,
+		TokenTTL: tokenTTL,
+		provider: analyseDataProvider,
+	}
+}
+
+func (r *RedisService) SetAnalysedData(ctx context.Context, dataTitle string, analysedData interface{}) error {
+	const op = "Cache_Service.internal.services.auth.SetAnalysedData"
+
+	err := r.provider.SetValue(ctx, dataTitle, analysedData, r.TokenTTL)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	r.log.Info("SetAnalysedData", slog.String("dataTitle", dataTitle))
+	return nil
+
+}
+
+// TODO: ревлизовать метод, вызывающий GetValue из AnasyledDataProvider
+func (r *RedisService) GetAnalysedData(ctx context.Context, dataTitle string) (interface{}, error) {
+	return nil, nil
+}
