10a230021915e16067507e7cf5a0073c653e6369
FrogaIo <nickosipovosipov@yandex.ru>
2026-03-22 17:24:07 +0300

    Added the secure API keys(deleted all of the old ones)

--- backend/Analitic_Service/AI_handler.py
+++ backend/Analitic_Service/AI_handler.py
@@ -1,6 +1,10 @@
 #import globals
 import threading
 
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
+
+
 from DeepSeek import answer as ds
 from GemmaAI import answer as ge
 from StepAI import answer as st 
@@ -9,11 +13,13 @@ from YandexAI import answer as ya
 #from AnaliticKafka import getMessage
 
 def AI_handler(context):
+    load_dotenv("API_Keys.env")
     AI_answer = {"ds": {}, "ge": {}, "st": {}, "ya":{}}
     AI_list = {"ds", "ge", "st", "ya" }
     for i in AI_list:
         try:
-            part = globals()[i](context).split("-%91%8FROG-COD", maxsplit = 1)
+            token = getenv(i.upper())
+            part = globals()[i](context, token).split("-%91%8FROG-COD", maxsplit = 1)
             AI_answer[i]["answer"] = part[0].replace(" \n", "")
             AI_answer[i]["reason"] = part[1]
         except:
