e823c0342fff0504bebb7c16311569c7a0533fc1
Dimitry Safronov <sda201018@gmail.com>
2026-03-25 21:48:59 +0300

    dirty version of pagination function

--- backend/Recommendation_Service/internal/storage/redis/redis.go
+++ backend/Recommendation_Service/internal/storage/redis/redis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"recommendationService/internal/domain/models"
 	"time"
 
@@ -28,41 +29,64 @@ func NewRedisController(addr string, password string, db int, protocol int, ttl 
 	}
 }
 
-func (r *RedisController) GetPostsByChannels(ctx context.Context, channels []string) ([]models.Post, error) {
+func (r *RedisController) GetPostsByChannels(ctx context.Context, channels []string, userID int64) ([]models.Post, error) {
 	const op = "Recommendation_Service.internal.storage.redis.GetPostsByChannelsAndDate"
 
-	tmpKey := "post:" + uuid.NewString()
-	defer r.redis.Del(ctx, tmpKey)
-
-	postKeys := make([]string, 0, len(channels))
-	for _, channel := range channels {
-		postKeys = append(postKeys, "post:channel:"+channel)
-	}
-
-	_, err := r.redis.ZUnionStore(ctx, tmpKey, &redis.ZStore{Keys: postKeys}).Result()
+	feedKey := fmt.Sprintf("feed:%d", userID)
+	cursorKey := fmt.Sprintf("feed_cursor:%d", userID)
+	exists, err := r.redis.Exists(ctx, feedKey).Result()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	ids, err := r.redis.ZRevRangeByScore(ctx, tmpKey, &redis.ZRangeBy{
-		Min: "-inf",
-		Max: "+inf",
+	if exists == 0 {
+		postKeys := make([]string, 0, len(channels))
+		for _, channel := range channels {
+			postKeys = append(postKeys, fmt.Sprintf("post:channel:%s", channel))
+		}
+		_, err := r.redis.ZUnionStore(ctx, feedKey, &redis.ZStore{
+			Keys: postKeys,
+		}).Result()
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		r.redis.Expire(ctx, feedKey, r.DefaultTTl)
+	}
+
+	lastScore, err := r.redis.Get(ctx, cursorKey).Float64()
+	if err == redis.Nil {
+		lastScore = math.Inf(1)
+	} else if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	ids, err := r.redis.ZRevRangeByScore(ctx, feedKey, &redis.ZRangeBy{
+		Min:    "-inf",
+		Max:    fmt.Sprint("%f", lastScore),
+		Offset: 0,
+		Count:  2,
 	}).Result()
 	if err != nil {
-		return []models.Post{}, fmt.Errorf("%s: %w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	if len(ids) == 0 {
+		return []models.Post{}, nil
+	}
+	lastId := ids[len(ids)-1]
+	score, err := r.redis.ZScore(ctx, feedKey, lastId).Result()
+	if err == nil {
+		r.redis.Set(ctx, cursorKey, score, time.Hour)
 	}
 
-	keys := make([]string, len(ids))
-	for i, id := range ids {
-		keys[i] = "post:" + id
+	keys := make([]string, 0, len(ids))
+	for _, id := range ids {
+		keys = append(keys, fmt.Sprintf("post:%s", id))
 	}
-
 	vals, err := r.redis.MGet(ctx, keys...).Result()
 	if err != nil {
-		return []models.Post{}, fmt.Errorf("%s: %w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	posts := make([]models.Post, 0, len(vals))
 
+	posts := make([]models.Post, 0, len(keys))
 	for _, val := range vals {
 		if val == nil {
 			continue
@@ -70,10 +94,11 @@ func (r *RedisController) GetPostsByChannels(ctx context.Context, channels []str
 		var post models.Post
 		err := json.Unmarshal([]byte(val.(string)), &post)
 		if err != nil {
-			continue
+			return nil, fmt.Errorf("%s: %w", op, err)
 		}
 		posts = append(posts, post)
 	}
+
 	return posts, nil
 }
 
