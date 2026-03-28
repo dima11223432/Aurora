7c662cf69b47a7231c18ed428a114aeb2e25731b
Dimitry Safronov <sda201018@gmail.com>
2026-03-28 21:58:44 +0300

    i have just fixed bug with dublicated data in redis yeah))

--- backend/Cache_Service/internal/storage/redis/redis.go
+++ backend/Cache_Service/internal/storage/redis/redis.go
@@ -4,6 +4,7 @@ import (
 	"CacheService/internal/domain/models"
 	"CacheService/internal/storage"
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -34,10 +35,24 @@ func (r *RedisController) SetCard(ctx context.Context, value models.AnalysedData
 	const op = "Cache_Service.internal.storage.redis.SetCard"
 	id := uuid.NewString()
 	timestamp := value.Date.Unix()
+	postTextHash := sha256.Sum256([]byte(value.PostText))
+	valueToken := fmt.Sprintf("post:unique:%x", postTextHash)
+
+	created, err := r.redis.SetNX(ctx, valueToken, "unique", r.DefaultTTl).Result()
+
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if !created {
+		return nil
+	}
+
 	pipeline := r.redis.TxPipeline()
 
 	data, err := json.Marshal(value)
 	if err != nil {
+		r.redis.Del(ctx, valueToken)
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
@@ -59,6 +74,7 @@ func (r *RedisController) SetCard(ctx context.Context, value models.AnalysedData
 
 	_, err = pipeline.Exec(ctx)
 	if err != nil {
+		r.redis.Del(ctx, valueToken)
 		return err
 	}
 	return nil
