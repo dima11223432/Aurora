d632efe8b6a8d8d2607dc05537c4dfffdc7966f1
Dimitry Safronov <sda201018@gmail.com>
2026-04-10 22:23:32 +0300

    i have added class standart tests on auth_test

--- backend/SSO/internal/services/auth_test.go
+++ backend/SSO/internal/services/auth_test.go
@@ -9,81 +9,100 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/suite"
 )
 
-func setupStorage(t *testing.T) (*postgres.Storage, func()) {
-
-	s, err := postgres.New("postgres://postgres:pass@localhost:5432/test_auth?sslmode=disable")
-	require.NoError(t, err)
-
-	teardown := func() {
-		s.DB.Exec("TRUNCATE TABLE users, channels, apps RESTART IDENTITY CASCADE")
-		s.DB.Close()
-	}
-	return s, teardown
-
+type AuthTestSuite struct {
+	suite.Suite
+	storage *postgres.Storage
+	service *auth.Auth
 }
 
-func TestLoginUser(t *testing.T) {
-	storage, teardown := setupStorage(t)
-	defer teardown()
+func (s *AuthTestSuite) SetupTest() {
+	storage, err := postgres.New("postgres://postgres:pass@localhost:5432/test_auth?sslmode=disable")
+	s.Require().NoError(err)
 
-	service := auth.New(slog.Default(), storage, storage, storage, time.Duration(5*time.Minute))
+	s.storage = storage
+	s.service = auth.New(slog.Default(), storage, storage, storage, 5*time.Minute)
 
-	storage.DB.Exec("INSERT INTO apps (id, name, secret) VALUES (1, 'test', 'secret')")
+	s.storage.DB.Exec("INSERT INTO apps (id, name, secret) VALUES (1, 'test', 'secret')")
+}
 
+func (s *AuthTestSuite) TearDownTest() {
+	s.storage.DB.Exec("TRUNCATE TABLE users, channels, apps RESTART IDENTITY CASCADE")
+	s.storage.DB.Close()
+}
+
+func (s *AuthTestSuite) TestLoginUser() {
 	testCases := []struct {
+		name    string
 		user    models.User
-		WithErr bool
+		withErr bool
 	}{
 		{
+			name: "Valid user",
 			user: models.User{
-
 				Telegram_id: 123456789,
 				First_name:  "Dima",
 				Last_name:   "Dmitriev",
 				Username:    "dimadmitriev",
 				Is_admin:    false,
 			},
-			WithErr: false,
+			withErr: false,
 		},
-
 		{
+			name: "Invalid Telegram ID",
 			user: models.User{
-
 				Telegram_id: 0,
 				First_name:  "Dima",
-				Last_name:   "Dmitriev",
-				Username:    "dimadmitriev",
-				Is_admin:    false,
 			},
-			WithErr: true,
-		},
-
-		{
-			user: models.User{
-
-				Telegram_id: 123456789,
-				First_name:  "Dima",
-				Last_name:   "",
-				Username:    "dimadmitriev",
-				Is_admin:    false,
-			},
-			WithErr: true,
+			withErr: true,
 		},
 	}
 
 	for _, tc := range testCases {
-		token, err := service.Login(context.Background(), tc.user, 1)
-		if tc.WithErr {
-			assert.Error(t, err)
-			assert.Empty(t, token)
-		} else {
-			assert.NoError(t, err)
-			assert.NotEmpty(t, token)
-		}
+		s.Run(tc.name, func() {
+			token, err := s.service.Login(context.Background(), tc.user, 1)
+			if tc.withErr {
+				s.Error(err)
+				s.Empty(token)
+			} else {
+				s.NoError(err)
+				s.NotEmpty(token)
+			}
+		})
+	}
+}
+
+func (s *AuthTestSuite) TestSetPriorityChannels() {
+	user := models.User{
+		Telegram_id: 123456789,
+		First_name:  "Dima",
+		Last_name:   "Dmitriev",
+		Username:    "dimadmitriev",
+		Is_admin:    false,
 	}
 
+	token, err := s.service.Login(context.Background(), user, 1)
+	s.NoError(err)
+	s.NotEmpty(token)
+
+	parser := jwt.NewParser(jwt.WithoutClaimsValidation())
+
+	claims := jwt.MapClaims{}
+	_, err = parser.ParseWithClaims(token, &claims, func(token *jwt.Token) (any, error) {
+		return []byte("secret"), nil
+	})
+
+	id := int64(claims["id"].(float64))
+	s.NoError(err)
+	s.NotZero(claims["id"])
+
+	err = s.service.SetPriorityChannels(context.Background(), id, []string{"channel1", "channel2"})
+	s.NoError(err)
+}
+
+func TestAuthSuite(t *testing.T) {
+	suite.Run(t, new(AuthTestSuite))
 }
