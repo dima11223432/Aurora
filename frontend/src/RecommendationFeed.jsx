a69b7c6a7d09efcb615ec95b05aaa78e500c2ff3
Dimitry Safronov <sda201018@gmail.com>
2026-04-16 21:32:36 +0300

    add little stilizations

--- frontend/src/RecommendationFeed.jsx
+++ frontend/src/RecommendationFeed.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import RecommendationCard from "./RecommendationCard";
-export default function RecommendationFeed(){
+export default function RecommendationFeed() {
   const [recommendatedPosts, setRecommendedPosts] = useState([]);
   const [cursor, setCursor] = useState({
-    score:0,
-    id:"",
+    score: 0,
+    id: "",
   });
   useEffect(() => {
     const login = async () => {
@@ -27,35 +27,35 @@ export default function RecommendationFeed(){
 
     login();
   }, []);
-  useEffect(()=>{
+  useEffect(() => {
     const fetchPosts = async () => {
-    try {
-      const token = localStorage.getItem("token");
+      try {
+        const token = localStorage.getItem("token");
 
-      const response = await axios.post(
-        "http://localhost:8081/v1/get_recommendated_posts",
-        { cursor },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
+        const response = await axios.post(
+          "http://localhost:8081/v1/get_recommendated_posts",
+          { cursor },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
           },
-        },
-      );
+        );
 
-      setRecommendedPosts((prev) => [...prev, ...response.data.posts]);
-      setCursor(response.data.nextCursor);
-      console.log(response.data);
-    } catch (e) {
-      console.error(e);
-    }};
+        setRecommendedPosts((prev) => [...prev, ...response.data.posts]);
+        setCursor(response.data.nextCursor);
+        console.log(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
     fetchPosts();
-  },[])
-  return(
-  <div>
-      {recommendatedPosts.map((post, index)=>(
-      <RecommendationCard key={index}{...post}/>
+  }, []);
+  return (
+    <div className="min-h-screen flex flex-col gap-6 bg-gradient-to-br from-[#0A0F1F] via-[#0F1A2F] to-[#02B7DB] flex items-center justify-center p-4 sm:p-6">
+      {recommendatedPosts.map((post, index) => (
+        <RecommendationCard key={index} {...post} />
       ))}
-      
     </div>
   );
 }
