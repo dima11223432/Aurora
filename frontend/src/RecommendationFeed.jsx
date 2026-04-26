b56defe9ae2357660fcb304de5c3e7d8f19405a5
Dimitry Safronov <sda201018@gmail.com>
2026-04-26 15:53:00 +0300

    i have added shtora again

--- frontend/src/RecommendationFeed.jsx
+++ frontend/src/RecommendationFeed.jsx
@@ -94,7 +94,7 @@ export default function RecommendationFeed() {
 
         {isLoading && <p className="text-white">Загрузка...</p>}
       </div>
-
+      <Shtora />
       <Footer />
     </div>
   );
