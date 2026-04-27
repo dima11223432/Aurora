db5c600bc954d07a17d24c6bffe2f9a68c0c08f1
Dimitry Safronov <sda201018@gmail.com>
2026-04-27 19:46:12 +0300

    add new storage method

--- backend/Recommendation_Service/internal/storage/postgres/postgres.go
+++ backend/Recommendation_Service/internal/storage/postgres/postgres.go
@@ -87,6 +87,15 @@ func (s *Storage) GetAllParsingChannels(ctx context.Context) ([]string, error) {
 
 func (s *Storage) AddNewUserCustomParsingChannel(ctx context.Context, userID int64, channel string) error {
 	const op = "internal.storage.postgres.AddNewUserCustomParsingChannel"
+	q := `INSERT INTO user_custom_parsing_channels (user_id, channel_username) VALUES ($1, $2)`
+
+	if _, err := s.parserDB.ExecContext(ctx, q, userID, channel); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	s.AddOnlyNewParsingChannel(ctx, channel)
+
+	return nil
 }
 
 func (s *Storage) AddOnlyNewParsingChannel(ctx context.Context, channel string) error {
