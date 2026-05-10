7489467bfdb32e9c4d04ce63b38bde19ee258670
Dimitry Safronov <sda201018@gmail.com>
2026-05-10 22:31:09 +0300

    i have removed footer

--- frontend/src/RecommendationFeed.jsx
+++ frontend/src/RecommendationFeed.jsx
@@ -100,7 +100,6 @@ export default function RecommendationFeed() {
         {isLoading && <p className="text-white">Загрузка...</p>}
       </div>
       <Shtora />
-      <Footer />
     </div>
   );
 }
