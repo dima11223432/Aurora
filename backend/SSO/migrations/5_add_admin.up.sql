04fadafa95f1ea70f5af7a33ecfa18f64e306818
Dimitry Safronov <sda201018@gmail.com>
2026-05-12 20:25:02 +0300

    i have adde delete migration

--- backend/SSO/migrations/5_add_admin.up.sql
+++ backend/SSO/migrations/5_add_admin.up.sql
@@ -1,3 +1,7 @@
-INSERT into users(user_id, telegram_id, username, first_name, last_name, is_admin)
+INSERT INTO users(user_id, telegram_id, username, first_name, last_name, is_admin)
 VALUES (1, 123456789, 'john_doe', 'john', 'doe', true)
-ON CONFLICT DO NOTHING;
+ON CONFLICT (user_id) 
+DO UPDATE SET 
+    username = EXCLUDED.username,
+    first_name = EXCLUDED.first_name,
+    is_admin = EXCLUDED.is_admin;
