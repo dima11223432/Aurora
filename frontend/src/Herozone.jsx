895961d21c5ec8afb6c97287efda20ce56250a90
Dimitry Safronov <sda201018@gmail.com>
2026-05-14 20:19:06 +0300

    i have added error page

--- frontend/src/Herozone.jsx
+++ frontend/src/Herozone.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_BASE_URL_SECURE, routes } from "./config/api";
+import logo from "./assets/Aurora.png";
 
 function Herozone() {
   const [isLoading, setIsLoading] = useState(false);
@@ -27,6 +28,7 @@ function Herozone() {
 
     if (!user || !user.id) {
       console.error("Данные пользователя не найдены");
+      navigate("/404");
       setTimeout(() => {
         setError("Не удалось получить данные пользователя");
 
@@ -35,7 +37,6 @@ function Herozone() {
       return;
     }
 
-    console.log("Telegram user data:", user);
     setTimeout(() => {
       setIsLoading(true);
     }, 0);
@@ -57,7 +58,7 @@ function Herozone() {
       .then((res) => res.json())
       .then((data) => {
         console.log("JWT Token получен");
-        localStorage.setItem("jwt", data.token);
+        localStorage.setItem("token", data.token);
       })
       .catch((err) => {
         console.error("Login API error:", err);
@@ -92,16 +93,12 @@ function Herozone() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0A0F1F] via-[#0F1A2F] to-[#02B7DB] flex items-center justify-center p-4 sm:p-6">
       <div className="max-w-3xl w-full bg-[rgba(20,25,50,0.7)] backdrop-blur-md rounded-[3rem] p-6 sm:p-8 md:p-12 shadow-2xl border border-white/5">
-        <img
-          src="/Aurora-logo.png"
-          alt="Aurora logo"
-          className="w-20 h-20 mb-2 mx-auto"
-        />
+        <img src={logo} alt="Aurora logo" className="w-20 h-20 mb-2 mx-auto" />
 
         <p className="text-[#95bec7] text-base text-center sm:text-lg mb-8 sm:mb-12 pl-4 to-transparent">
           Добро пожаловать в{" "}
           <span className="text-[#0fd2f5] font-bold">Aurora</span>
-          <br /> ИИ-поддерживаемые прогнозы TON и T-Investments
+          <br /> ИИ-поддерживаемые прогнозы в реальном времени
         </p>
 
         <div className="space-y-4 sm:space-y-5 mb-8 sm:mb-12">
