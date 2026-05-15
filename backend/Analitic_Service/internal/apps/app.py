c56561c24cc7c8b38a66c6973d113e68afd2bd9e
Dimitry Safronov <sda201018@gmail.com>
2026-05-15 23:47:42 +0300

    i have connected analytic service

--- backend/Analitic_Service/internal/apps/app.py
+++ backend/Analitic_Service/internal/apps/app.py
@@ -69,7 +69,7 @@ class App:
                     else:
                         context_text = str(payload)
                     self.logger.info(f"Received message from {topic}: {context_text}")
-                    # result = AI_handler(context_text)
+                    result = AI_handler(context_text)
                     result = redact_recursive(result)
                     self.logger.info(f"Received AI result: {result}")
                     ai_data = result.get("ds", {})
@@ -99,7 +99,7 @@ class App:
                     self.logger.info(f"Final payload: {final_json}")
                     # send_payload = {"original_offset": msg.offset(), "result": result}
                     # NOTE: send payload to kafka
-                    # kafka_controller.send_batch_messages({result_topic: send_payload})
+                    kafka_controller.send_batch_messages({result_topic: send_payload})
                     self.logger.info(f"Sent AI result to {result_topic}")
 
                 except Exception as e:
