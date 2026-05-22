fd4136fc29327187615fe1ba67f56c3a57df9d4f
Dimitry Safronov <sda201018@gmail.com>
2026-05-22 21:35:33 +0300

    i have changed block buf size

--- backend/Recommendation_Service/internal/services/user_data_provider/userDataProvider.go
+++ backend/Recommendation_Service/internal/services/user_data_provider/userDataProvider.go
@@ -91,7 +91,7 @@ func (u *UserDataProvider) GetRecommendatedPosts(ctx context.Context, userID int
 		channelNames = append(channelNames, ch.Channel)
 	}
 
-	posts, nextCursor, err := u.priorityNewsProvider.GetPostsByChannels(ctx, channelNames, userID, cursor, 5)
+	posts, nextCursor, err := u.priorityNewsProvider.GetPostsByChannels(ctx, channelNames, userID, cursor, 15)
 	if err != nil {
 		return nil, nil, fmt.Errorf("%s: %w", op, err)
 	}
