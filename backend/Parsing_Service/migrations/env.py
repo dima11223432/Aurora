55f9b72764c5b28441b15ff4a4aa982a2d3bc4e9
Dimitry Safronov <sda201018@gmail.com>
2026-05-25 13:55:04 +0300

    i have edited env.py in alembic

--- backend/Parsing_Service/migrations/env.py
+++ backend/Parsing_Service/migrations/env.py
@@ -20,10 +20,11 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# Allow DATABASE_URL env var to override the ini file setting
+import os
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 
 def run_migrations_offline() -> None:
